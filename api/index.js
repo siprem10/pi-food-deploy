9823c7055082d49997a4f36543511c836b6a0d65
siprem10 <siprem10@gmail.com>
2022-08-18 03:29:29 -0300

    pre

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-/* const { preChargeRecipes, preChargeDiets } = require("./src/routes/precharge.js"); */
+const { preChargeRecipes, preChargeDiets } = require("./src/routes/precharge.js");
 require("dotenv").config();
 
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-    /* preChargeDiets(); // lo desactivo
-    preChargeRecipes(); // lo desactivo */
+    preChargeDiets(); // lo desactivo
+    preChargeRecipes(); // lo desactivo
     server.listen(process.env.PORT, () => {
         console.log(`%s listening at ${process.env.PORT}`);
     });
