edfe0e14a621f8cc8667ab2008581a610c50fd92
siprem10 <siprem10@gmail.com>
2022-11-09 17:00:08 -0300

    fix(UI in react new version)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./components/styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import Home from "./components/Home/Home.jsx";
@@ -8,17 +7,21 @@ import EditRecipe from "./components/EditRecipe/EditRecipe";
 import About from "./components/About/About";
 import NotFound from "./components/NotFound/NotFound";
 
+import "./components/styles/App.css";
+
 function App() {
     return (
-        <Routes>
-            <Route exact path="*" element={ <NotFound subtitle={"Page not found!"} /> } />
-            <Route exact path="/" element={ <LandingPage /> } />
-            <Route exact path="/home" element={ <Home/> } />
-            <Route exact path="/home/:id" element={ <Detail /> } />
-            <Route exact path="/form" element={ <Form /> } />
-            <Route exact path="/edit/:id" element={ <EditRecipe /> } />
-            <Route exact path="/about" element={ <About /> } />
-        </Routes>
+        <div className="App">
+            <Routes>
+                <Route exact path="*" element={ <NotFound subtitle={"Page not found!"} /> } />
+                <Route exact path="/" element={ <LandingPage /> } />
+                <Route exact path="/home" element={ <Home/> } />
+                <Route exact path="/home/:id" element={ <Detail /> } />
+                <Route exact path="/form" element={ <Form /> } />
+                <Route exact path="/edit/:id" element={ <EditRecipe /> } />
+                <Route exact path="/about" element={ <About /> } />
+            </Routes>            
+        </div>
     );
 }
 
